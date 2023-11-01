601e32f8663abdab5b633eaaf0dfd6c984f65f3d
Juan Manuel <juanitopilot01@gmail.com>
2023-10-31 20:34:52 -0700

    Estilos-sweet-toast
    se agregaron unas librerias

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,4 +1,6 @@
-
+import Swal from 'sweetalert2'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
@@ -6,11 +8,38 @@ export default function Paciente({ paciente, setPaciente, eliminarPaciente }) {
 
     const { nombre, propietario, email, fecha, sintomas, id } = paciente
 
-    const handleEliminar = () =>{
-        const respuesta = confirm('Deseas Eliminar este Paciente')
-        if(respuesta){
-                eliminarPaciente(id) 
-        }
+    const handleEliminar = () => {
+        Swal.fire({
+            title: `Estas Seguro de eliminar al Paciente ${nombre}`,
+            text: "Esta accion no se puede revertir",
+            icon: 'error',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Eliminar',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed){
+                toast.success('Paciente Eliminado Correctamente', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: false,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light"
+                })
+
+                
+            }
+            if(result.isConfirmed){
+                eliminarPaciente(id)
+            }
+            
+        })
+        
+
     }
     return (
         <div className=' m-5 bg-white shadow-md rounded-xl  px-5 py-10'>
@@ -65,9 +94,12 @@ export default function Paciente({ paciente, setPaciente, eliminarPaciente }) {
                     type="button"
                     className="bg-red-700 py-2 px-10 rounded-lg text-white font-bold hover:bg-red-900"
                     onClick={handleEliminar}
-                >Eliminar</button>
+                >Eliminar
+
+                </button>
 
             </div>
+            <ToastContainer />
         </div>
     )
 }

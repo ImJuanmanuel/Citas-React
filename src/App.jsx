73717980eb0b474b178031bbeabe800437fe4991
Juan Manuel <juanitopilot01@gmail.com>
2023-10-31 21:06:44 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react"
 import Header from "./components/Header"
 import Formulario from "./components/Formulario"
 import ListadoPacientes from "./components/ListadoPacientes"
+
 function App() {
 
   const [pacientes, setPacientes] = useState([])
